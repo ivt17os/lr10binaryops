692eee1dcb088a1c3b0b957987e18309c63bf40b
LVOV <xaata3007@mail.ru>
2019-02-11 09:26:08 +0900

    task 1 done

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -1,10 +1,12 @@
 	#include <stdio.h>
-
+// LVOV
 	int main(){
 
 		int x = 31;
-	
-		printf("%d\n", x<<1);
-
+		for(int i = 1; i <= 32; i++){
+			x = x << 1;
+			printf("%d\n", x);
+		}
+		//cсдвиг не циклический пушто не возвращается 
 		return 0;
 	}
