291c132a1d1d310e3a47e23640bc16c64ec63c35
Tudupov Dugar <CreedDT@gmail.com>
2019-02-11 09:29:25 +0900

    task 1 done

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -1,11 +1,16 @@
 	#include <stdio.h>
 
 	// Tudupov
+
 	int main(){
 
-		int x = 31;
+		int x = 31,i;
 	
-		printf("%d\n", x<<1);
+		for (i=1; i<=32; i++) {
+			x = x << 1;
+			printf("%d\n", x);
+		}
 
+		// Сдвиг не является циклическим, потому что не возврощается.
 		return 0;
 	}
