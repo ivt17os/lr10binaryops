12c996f8891ac3449d1951c1743f6c70322a51af
Студент <fedyad1997@mail.ru>
2019-02-11 09:28:36 +0900

    task1

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -2,9 +2,13 @@
 
 	int main(){
 
-		int x = 31;
-	
-		printf("%d\n", x<<1);
+		int x = 31; i=0;
+		
+		for(i=1; i<=32;i++){
+			x=x<<1;
+			printf("%d\n", x<<1);
+		}
+		//Сдвиг цикличен
 
 		return 0;
 	}
