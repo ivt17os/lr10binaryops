dd851dfb44501c8181c09bfe8312b9e66e34e37c
Student <kovit97@gmail.com>
2019-02-15 12:22:55 +0900

    zadanie 1

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -1,10 +1,13 @@
-	#include <stdio.h>
+//kononov
+#include <stdio.h>
 
 	int main(){
 
 		int x = 31;
-	
+	for(int i = 0; i < 100; i++){
+		x = x << 1;
 		printf("%d\n", x<<1);
-
+	}
 		return 0;
+		// ВЫВОД: << является обычным сдвигом
 	}
