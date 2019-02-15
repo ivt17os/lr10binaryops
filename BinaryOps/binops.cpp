4e2512e6114482387490ea936ca9f0525038a4ee
kralyatron <budin.vadim@mail.ru>
2019-02-15 12:22:24 +0900

    <<

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -1,10 +1,16 @@
 	#include <stdio.h>
+#include <iostream>
+using namespace std;
 	//Veremeenko
 	int main(){
 
 		int x = 31;
 	
-		printf("%d\n", x<<1);
-
+		//printf("%d\n", x<<1);
+		for(int i=0; i <32; i++) {
+			x = x << 1;
+			cout << x << endl;
+		}
+		// Вывод: "<<" является нециклическим 
 		return 0;
 	}
