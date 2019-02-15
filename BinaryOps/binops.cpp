b9dbed0b4901eeaa9eaee7eb283ea0a7bdcf8f66
Студент <study.svfu@gmail.com>
2019-02-15 12:23:43 +0900

    Z1

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -4,9 +4,14 @@
 
 	int main(){
 
-		int x = 31;
+		int i=0, x = 31;
 	
-		printf("%d\n", x<<1);
+		for (i=0; i<100; i++) {
+			x = x + 1;
+			printf("%d\n", x<<1);
+		}
 
 		return 0;
+	
+		//ВЫВОД: Не циклический сдвиг 
 	}
