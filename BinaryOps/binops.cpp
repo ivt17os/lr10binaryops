790599ca9547a8d3928fb38e74b508ba084c9855
ZakharovYY <speedhunter753@gmail.com>
2019-02-22 13:17:22 +0900

    task3

--- BinaryOps/binops.cpp
+++ BinaryOps/binops.cpp
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-	int main1(){
+	int main2(){
 
 		int x = 31;
 	for(int i = 0; i < 100; i++){
--- BinaryOps/task3.cpp
+++ BinaryOps/task3.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+	#include <iostream>
 #include <string>
 #include <sstream>
 
@@ -26,14 +26,24 @@ int main(){
 	ssmask >> mb[0] >> ch >> mb[1] >> ch >> mb[2] >> ch >> mb[3];
 
 	// вычислите адреса:
-
+	char negmask[4];
+	for(int i=0; i<4; i++){
+		negmask[i] = mb[i];
+		negmask[i] = ~negmask[i];
+	}
 	// обнулить те биты, которые равны нулю в маске
 	// это будет адрес подсети
-	cout << "\n";
+	cout << (ipb[0]&mb[0]) << "." <<
+			(ipb[1]&mb[1])<< "." <<
+			(ipb[2]&mb[2])<< "." <<
+			(ipb[3]&mb[3])<< "\n";
 
 	// обнулить те биты, которые равны нулю в маске
 	// это будет широковещательный адрес подсети
-	cout << "\n";
+	cout << (unsigned)(ipb[0]| negmask[0]) << "." <<
+			(unsigned)(ipb[1]| negmask[1])<< "." <<
+			(unsigned)(ipb[2]| negmask[2])<< "." <<
+			(unsigned)(ipb[3]| negmask[3])<< "\n";
 
 	return 0;
 }
