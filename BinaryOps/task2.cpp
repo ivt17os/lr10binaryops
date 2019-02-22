3ef4546ff8d4a0f5038cc4b65c6dc24f0d2ed321
stepandanilov <5digitnoob@gmail.com>
2019-02-22 12:27:20 +0900

    task2 done

--- BinaryOps/task2.cpp
+++ BinaryOps/task2.cpp
@@ -14,9 +14,10 @@ int main() {
  
     do {
         if ((res.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0 &&
+			(res.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) != 0 &&
 			_tcscmp(res.cFileName, TEXT("..")) != 0 &&
 			_tcscmp(res.cFileName, TEXT(".")) != 0) { // если это подпапка
-				_tprintf("%s\n"TEXT(res.cFileName));
+				_tprintf(TEXT("%s\n"),res.cFileName);
         }
     } while (FindNextFile(hFind, &res) != 0);
     FindClose(hFind);
